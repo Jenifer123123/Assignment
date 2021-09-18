e7834957fa44264674221782a84c8cd7bed5049d
Jenifer123123 <66411119+Jenifer123123@users.noreply.github.com>
2021-09-18 22:45:11 +0530

    DAY 1 ASSIGNMENT SUBMITTED

---
+++ DAY 1 - .2nd program.py
@@ -0,0 +1,3 @@
+print(1>3 >4)
+# First it checks if 1 is greater than 3 and then checks if 3 is greater than 4.
+#Here if both the condition is true it prints true, else print false
---
+++ DAY 1 - 1st program.py
@@ -0,0 +1,10 @@
+a = 5.5
+b = 5.5
+print(a == b)
+print(a is b)
+# both the values stored in same address
+s1 = "JENI"
+s2 = "JENI"
+print(s1 == s2)
+print(s1 is s2)
+#both are pointed in same address
